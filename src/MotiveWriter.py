7498bd8305a933cfe71004eb33b89f735a8c9a75
kellerer <jkellerer@posteo.de>
2024-05-24 20:01:18 +0200

    feat: add script to analyze result from motive finder

--- src/MotiveWriter.py
+++ src/MotiveWriter.py
@@ -1,11 +1,8 @@
 import json
 from dataclasses import asdict
 from pathlib import Path
-from typing import List
 
-from Motive import Motive
 from MotiveList import MotiveList
-from SequenceType import SequenceType
 
 
 def write_motives_as_json_to_file(motives: MotiveList, output_folder: Path):
@@ -20,48 +17,3 @@ def write_motives_as_json_to_file(motives: MotiveList, output_folder: Path):
 
 output_filename = "output.csv"
 output_json_filename = "output.json"
-
-
-def write_motives_to_file(motives: List[Motive], output_folder: Path):
-
-    if not output_folder.exists():
-        output_folder.mkdir()
-
-    with open(output_folder / output_filename, "w") as file:
-        file.write(
-            "work\tsequence\tfrequency\tpositions\tfrequency_original\tpositions_original\tfrequency_inverted\tpositions_inverted\tfrequency_mirrored\tposition_mirrored\n"
-        )
-        for motive in motives:
-            original_positions = list(
-                filter(
-                    lambda pos: pos.position_in_work == SequenceType.ORIGINAL,
-                    motive.positions,
-                )
-            )
-
-            inverted_positions = list(
-                filter(
-                    lambda pos: pos.position_in_work == SequenceType.INVERTED,
-                    motive.positions,
-                )
-            )
-            mirrored_positions = list(
-                filter(
-                    lambda pos: pos.position_in_work == SequenceType.MIRRORED,
-                    motive.positions,
-                )
-            )
-
-            output: List[str] = []
-            output.append(motive.sequence[0].origin.piece_title)
-            output.append(str(motive.sequence))
-            output.append(str(motive.frequency))
-            output.append(str(motive.positions))
-            output.append(str(len(original_positions)))
-            output.append(str(original_positions))
-            output.append(str(len(inverted_positions)))
-            output.append(str(inverted_positions))
-            output.append(str(len(mirrored_positions)))
-            output.append(str(mirrored_positions))
-
-            file.write("\t".join(output) + "\n")
